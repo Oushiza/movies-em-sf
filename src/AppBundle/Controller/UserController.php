6223bf99e913999a6ba4a44ba4478dcfc8910e91
kevin <kevinlay.fr@gmail.com>
2015-01-08 13:00:16 +0100

    affichage du formulaire

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,13 +5,54 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\RegistrationType;
+
 class UserController extends Controller
 {
     /**
      * @Route("/register", name="register")
      */
-    public function registerAction()
-    {
-        return $this->render('user/register.html.twig');
+    public function registerAction(Request $request){
+        
+        //on crée un utilisateur vide (une instance de notre entité User)
+        $user = new User();
+        
+        //on récupère une instance de notre formulaire
+        //ce form est associé ) l'utilisateur vide
+        $registrationForm = $this->createForm(new RegistrationType(), $user);
+        
+        //traite le formulaire
+        $registrationForm->handleRequest($request);
+        
+        //si les données sont valides
+        if($registrationForm->isValid()){
+            //hydrate les autres propriétés de notre User
+            
+                //hacher le mot de passe
+
+                //générer un salt
+                $salt = md5(uniqid());
+                $user->setSalt($salt);
+            
+                //générer un token
+                $token = md5(uniqid());
+                $user->setSalt($token);
+                
+                //date d'inscription et date de modification
+            
+            //sauvegarde le User en bdd
+            
+        }
+        
+        dump($user);
+        
+        $params = array(
+            "registrationForm" => $registrationForm->createView()
+        );
+        
+        return $this->render('user/register.html.twig', $params);
     }
 }
