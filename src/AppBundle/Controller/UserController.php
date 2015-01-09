def9875bf475e3e9e8a17d3d82ea0098b3728d6a
kevin <kevinlay.fr@gmail.com>
2015-01-09 11:22:37 +0100

    mise en place du login

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,9 +6,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\RegistrationType;
+
+
 
 class UserController extends Controller
 {
@@ -30,8 +33,6 @@ class UserController extends Controller
         //si les données sont valides
         if($registrationForm->isValid()){
             //hydrate les autres propriétés de notre User
-            
-                //hacher le mot de passe
 
                 //générer un salt
                 $salt = md5(uniqid());
@@ -39,11 +40,25 @@ class UserController extends Controller
             
                 //générer un token
                 $token = md5(uniqid());
-                $user->setSalt($token);
+                $user->setToken($token);
+                
+                //hacher le mot de passe
+                //sha512, 5000 fois
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $user->getPassword());
+                $user->setPassword($encoded);
                 
                 //date d'inscription et date de modification
+                $user->setDateRegistered(new \DateTime());
+                $user->setDateModified(new \DateTime());
+                
+                //assigne toujours ce rôle aux utilisateurs du front-office
+                $user->setRoles( array("ROLE_USER"));
             
             //sauvegarde le User en bdd
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
             
         }
         
@@ -54,5 +69,44 @@ class UserController extends Controller
         );
         
         return $this->render('user/register.html.twig', $params);
+    }
+    
+     /**
+     * @Route("/login", name="login_route")
+     */
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                Security::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $session->get(Security::AUTHENTICATION_ERROR);
+            $session->remove(Security::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(Security::LAST_USERNAME);
+
+        return $this->render(
+            'user/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
     }
 }
